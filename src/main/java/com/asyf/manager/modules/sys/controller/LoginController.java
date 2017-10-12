2b5e6e2e86a4bf9f0074bf6230c676bd0afe0368
373616511@qq.com <373616511@qq.com>
2017-10-12 11:19:09 +0800

    excel poi demo

---
+++ src/main/java/com/asyf/manager/modules/sys/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.asyf.manager.modules.sys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2017/10/12.
+ */
+@Controller
+@RequestMapping(value = "/login")
+public class LoginController {
+
+}
